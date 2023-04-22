4a6f5c192a802a24073ca05a5c6af18cc9a61df8
Vitaliy <fruitamin@yandex.ru>
2023-04-22 04:52:58 +0300

    Добавлен HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development'
 const devMode = mode === 'development'
@@ -11,8 +12,14 @@ module.exports = {
     devtool,
     entry: path.resolve(__dirname, 'src/js/main.js'),
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
+        path: path.resolve(__dirname, 'dist'),
         clean: true,
-        filename: "[name].[contenthash].js"
-    }
+        filename: "js/[name].[contenthash].js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+
+    ]
 }
